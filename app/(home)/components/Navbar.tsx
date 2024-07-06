552e3039ae4bcb04d8ef256b3a5ad7c2a513cb5c
antho <anthonyyoussef01@gmail.com>
2024-07-06 11:33:27 -0400

    Home Page Update:
    - Added the calla lily in the home page instead of that flower emoji

--- app/(home)/components/Navbar.tsx
+++ app/(home)/components/Navbar.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
 import { faLinkedin } from '@fortawesome/free-brands-svg-icons';
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import Image from "next/image";
 
 export default function Navbar() {
     const socials = [
@@ -17,12 +18,17 @@ export default function Navbar() {
         }
     ];
     return (
-        <nav className="py-10 flex justify-between items-center">
-            <h1 className="text-gray-200 text-2xl font-bold underline underline-offset-8 decoration-orange-400">Lily 🌺</h1>
+        <nav className="py-9 flex justify-between items-center">
+            <div className="flex items-center border-b-orange-400 border-b-2">
+                <h1 className="text-gray-200 text-2xl font-bold">
+                    Lily
+                </h1>
+                <Image src="/calla_lily.png" alt="🌺" width={38} height={38} className="ml-1 mb-1"/>
+            </div>
             <div className="text-gray-200 flex items-center gap-5">
                 {socials.map((social, index) => (
                     <a key={index} href={social.link} aria-label={social.label} className="hover:text-orange-100">
-                        <FontAwesomeIcon icon={social.icon} className="size-4" />
+                        <FontAwesomeIcon icon={social.icon} className="size-4"/>
                     </a>
                 ))}
                 {/*<DarkModeToggle />*/}
