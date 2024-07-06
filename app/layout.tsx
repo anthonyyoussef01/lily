48232f569926b107c7ba149bd526b4c441d51119
antho <anthonyyoussef01@gmail.com>
2024-07-06 11:15:37 -0400

    Favicon Update:
    - Added a favicon for the site

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,27 +6,43 @@ import {ThemeProvider} from "next-themes";
 const spaceGrotesk = Space_Grotesk({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Lily Marino",
-  description: "EMT & Aspiring Physician Assistant (PA)",
+    title: "Lily Marino",
+    description: "EMT & Aspiring Physician Assistant (PA)",
+    icons: [
+        {
+            rel: 'icon',
+            type: 'image/x-icon',
+            url: '/favicon.ico',
+        },
+        {
+            rel: 'apple-touch-icon',
+            url: '/calla_lily.png',
+        },
+        {
+            rel: 'icon',
+            type: 'image/png',
+            url: '/calla_lily.png',
+        }
+    ],
 };
 
 export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
+                                       children,
+                                   }: Readonly<{
+    children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en" className={`${spaceGrotesk.className}`} suppressHydrationWarning={true}>
-      <body>
-        <ThemeProvider
-            attribute="class"
-            defaultTheme="light"
-            enableSystem
-            disableTransitionOnChange
-        >
-          {children}
-        </ThemeProvider>
-      </body>
-    </html>
-  );
+    return (
+        <html lang="en" className={`${spaceGrotesk.className}`} suppressHydrationWarning={true}>
+            <body>
+                <ThemeProvider
+                    attribute="class"
+                    defaultTheme="light"
+                    enableSystem
+                    disableTransitionOnChange
+                >
+                    {children}
+                </ThemeProvider>
+            </body>
+        </html>
+    );
 }
